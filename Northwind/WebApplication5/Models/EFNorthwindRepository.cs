25def097ef870784ce86bb08a0079d27ed65f9b5
jayer1 <jayer1@my.wctc.edu>
2019-03-01 04:56:10 -0600

    Committed problem with pages

---
+++ Northwind/WebApplication5/Models/EFNorthwindRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Northwind.Models
+{
+    public class EFNorthwindRepository : INorthwindRepository
+    {
+        private NorthwindContext context;
+        public EFNorthwindRepository(NorthwindContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IQueryable<Product> Product => context.Product;
+
+        public IQueryable<Category> Category => context.Category;
+    }
+}
